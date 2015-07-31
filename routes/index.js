7a342d7724452596ed500fe9b490a422ed622fe3
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-07-31 14:42:35 -0600

    started board

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/game');
+});
+
+router.get('/game', function(req, res, next) {
+  res.render('game', { title: 'Express' });
 });
 
 module.exports = router;

c3796b543faa5dc623540d29abd9f4bed194da81
Claire Nessler <clrnessler@gmail.com>
2015-08-03 13:02:02 -0600

    playground

--- playground.js
+++ playground.js
@@ -1,37 +1,49 @@
-var deeds = require('./library/deedConstructor.js')()
-var playerFunctions = require('./library/playerMethods.js')()
-var bank = require('./library/bankConstructor.js')()
-
-console.log(playerFunctions.buyDeed);
-
-function Deed(name, price, color,  rent, owner, houses, hotels, mortgage, boardIndex) {
-  this.name = name;
-  this.price = price;
-  this.color = color;
-  this.rent = rent;
-  this.owner = owner;
-  this.houses = houses;
-  this.hotels = hotels;
-  this.mortgage = mortgage;
-  this.boardIndex = boardIndex;
-}
-var sp1 = new Deed('Colfax', 60 , 'brown', 2, null, 0, 0, 30, 1)
-
-// var player = new Player('you', 'ship')
-// // console.log(bank, 'HHHHHHH');
-// var buyDeed = playerFunctions.buyDeed(player, sp1, bank);
-// console.log(buyDeed);
-
-
-// var myObj = {
-//   a: function () {
-//   this.b = "hi"
-//   },
+// var shuffleDeck = function (deck) {
 //
-//   a.prototype.logger: function () {
-//     console.log(this.b);
+//  var shuffledDeck = [];
+//
+//  while(shuffledDeck.length < deck.length) {
+//    var randomIndex = Math.floor(Math.random()*deck.length);
+//    if(shuffledDeck.indexOf(deck[randomIndex]) === -1) {
+//      shuffledDeck.push(deck[randomIndex]);
+//    }
+//  }
+//  return shuffledDeck;
+// }
+//
+// var drawCard = function (deck, counter) {
+//   if(counter < deck.length - 1) {
+//     outputCard = deck[counter];
+//     return [outputCard, counter];
+//   }
+//   else {
+//     outputCard = deck[counter];
+//     deck = shuffleDeck(deck);
+//     return [outputCard, deck];
 //   }
 // }
 //
-// var instance = new a();
-// instance.logger()
+// var deck = ['a', 'b', 'c', 'd', 'e'];
+//
+// var counter = 0;
+// for (var i = 0; i < 20; i++) {
+//   var output = drawCard(deck, counter);
+//   console.log(output);
+//   if (typeof output[1] === "object") {
+//     counter = 0;
+//     deck = output[1];
+//   }
+//   else{
+//     counter++;
+//   }
+// }
+
+var logX = function (y) {
+  console.log(y);
+}
+
+var x = 5;
+for (var i = 0; i < 5; i++) {
+  logX(x);
+  x++;
+}

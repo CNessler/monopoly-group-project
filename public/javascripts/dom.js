e1f4a88899ca95296c3e7f9c68c7ace5d6277847
Claire Nessler <clrnessler@gmail.com>
2015-08-03 14:11:01 -0600

    dom.js working

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -8,23 +8,26 @@ var tokens = [
   {name: "car", url: 'http://4.bp.blogspot.com/-ogjy-t9xq1E/UQGnaJbui8I/AAAAAAAAB-k/OPDG4YAI2Yk/s1600/monopoly_token_car.png'}
 ]
 
-var atts = ["name", "token", "balance", "inJail", "getOut", "active", "turn", "deeds", "location"]
+function getToken(player) {
+  for (var i = 0; i < tokens.length; i++) {
+    if (player.token === tokens[i].name) {
+      player.tokensrc = tokens[i].url
+    }
+  }
+  return player
+}
+
 var players = [];
 var data = document.getElementById('playerData').childNodes;
 for (var i = 0; i < data.length; i++) {
-  var info = data[i].innerHTML.split(";");
-  var player = {};
-  for (var j = 0; j < info.length; j++) {
-    player[atts[j]] = info[j];
-  }
+  var singlePlayer = data[i].innerHTML.split(';');
+  var player = new Player(singlePlayer[0], singlePlayer[1])
+  getToken(player);
   player.location = 0;
-  for (var k = 0; k < tokens.length; k++) {
-    if (player.token === tokens[k].name) {
-      player.tokensrc = tokens[k].url;
-    }
-  }
-  players.push(player)
+  players.push(player);
 }
+console.log(player);
+console.log(players[0].name);
 
 function getMove(player) {
   var move = Math.floor(Math.random()*10) + 2;
@@ -46,8 +49,7 @@ function nextPlayer() {
 
 rollButton.addEventListener("click", function() {
   var player = players[index]
-  console.log(player.name);
-  console.log(player.tokensrc);
+  console.log(player, "INDEX");
   var current = document.getElementById('sp' + player.location)
   current.style.backgroundImage = null;
   var dieRoll = getMove(player);

a4171b6745bf0e2b01e21971e48a318c23b47f8c
Jaylyn Stoesz <jaylynstoesz@gmail.com>
2015-08-04 16:29:32 -0600

    font color bug

--- public/javascripts/dom.js
+++ public/javascripts/dom.js
@@ -4,83 +4,68 @@ var turn = document.getElementById('turn')
 var misc = [12, 28, 5, 15, 25, 35]
 
 var tokens = [
-  {name: "hat", url: 'http://www.worldofmonopoly.com/fansite/images/tokens/monopoly_token_hat.png'},
-  {name: "ship", url: 'http://www.worldofmonopoly.com/fansite/images/tokens/monopoly_token_ship.png'},
-  {name: "dog", url: 'https://gobeyondphotography.files.wordpress.com/2013/01/monopoly_token_dog.png'},
-  {name: "car", url: 'http://4.bp.blogspot.com/-ogjy-t9xq1E/UQGnaJbui8I/AAAAAAAAB-k/OPDG4YAI2Yk/s1600/monopoly_token_car.png'}
+ {name: "hat", url: 'http://www.worldofmonopoly.com/fansite/images/tokens/monopoly_token_hat.png'},
+ {name: "ship", url: 'http://www.worldofmonopoly.com/fansite/images/tokens/monopoly_token_ship.png'},
+ {name: "dog", url: 'https://gobeyondphotography.files.wordpress.com/2013/01/monopoly_token_dog.png'},
+ {name: "car", url: 'http://4.bp.blogspot.com/-ogjy-t9xq1E/UQGnaJbui8I/AAAAAAAAB-k/OPDG4YAI2Yk/s1600/monopoly_token_car.png'}
 ]
 
 function getToken(player) {
-  for (var i = 0; i < tokens.length; i++) {
-    if (player.token === tokens[i].name) {
-      player.tokensrc = tokens[i].url
-    }
-  }
-  return player
+ for (var i = 0; i < tokens.length; i++) {
+   if (player.token === tokens[i].name) {
+     player.tokensrc = tokens[i].url
+   }
+ }
+ return player
 }
 
 var players = [];
 var data = document.getElementById('playerData').childNodes;
 for (var i = 0; i < data.length; i++) {
-  var singlePlayer = data[i].innerHTML.split(';');
-  var player = new Player(singlePlayer[0], singlePlayer[1])
-  getToken(player);
-  player.location = 0;
-  players.push(player);
-}
-function getMove(player) {
-  var move = Math.floor(Math.random()*10) + 2;
-  // var move = 15;
-  var inEl = player.location + move;
-  if (inEl === 30){
-    alert("You're In The Elevator!")
-    player.inJail = true;
-    player.location = 10;
-    redirectToken(10, player);
-  }
-  else if (player.location + move < 40) {
-
+ var singlePlayer = data[i].innerHTML.split(';');
+ var player = new Player(singlePlayer[0], singlePlayer[1])
+ getToken(player);
+ player.location = 0;
+ players.push(player);
+ }
 
 function getMove(player) {
-  var move = Math.floor(Math.random()*10) + 2;
-  if (player.location + move < 40) {
-    player.location += move;
-  } else {
-    player.location = player.location + move - 40;
-    player.balance += 200;
-    bank.balance -= 200;
-  }
-  return move;
+ var move = Math.floor(Math.random()*10) + 2;
+ // var move = 7;
+ if (player.location + move < 40) {
+   player.location += move;
+ } else {
+   player.location = player.location + move - 40;
+   player.balance += 200;
+   bank.balance -= 200;
+ }
+ return move;
 }
 
 function nextPlayer() {
-  if (index <= 2) {
-    index += 1
-  } else if (index === 3) {
-    index = 0
-  }
-  // console.log(players[index].name, "PLAYERS TURN");
-  turn.innerHTML = players[index].name + "'s Turn!";
+ if (index <= 2) {
+   index += 1
+ } else if (index === 3) {
+   index = 0
+ }
+ turn.innerHTML = players[index].name + "'s Turn!";
 }
 
 rollButton.addEventListener("click", function() {
-
-  var player = players[index]
-  var current = document.getElementById('sp' + player.location)
-  current.style.backgroundImage = null;
-    for (var i = 0; i < misc.length; i++) {
-    var foo = "sp" + misc[i].toString();
-    if (current.id == foo) {
-      current.style.color = "white";
-    } else {
-      current.style.color = "black";
-    }
-  }
-  var dieRoll = getMove(player);
-  // console.log(player.location, "LOCATION");
-  var moveTo = document.getElementById('sp' + player.location)
-  moveTo.style.backgroundImage = "url('" + player.tokensrc + "')";
-  moveTo.style.color = "transparent";
-
-  selectPlayerFunction(player.location, player, bank, dieRoll, players, chanceDeck);
+ var player = players[index]
+ var current = document.getElementById('sp' + player.location)
+ current.style.backgroundImage = null;
+ current.style.color = "black";
+ for (var i = 0; i < misc.length; i++) {
+   var loc = "sp" + misc[i].toString()
+   if (current.id === loc) {
+     current.style.color = "white";
+   }
+ }
+ var dieRoll = getMove(player);
+ console.log(player.location, "LOCATION");
+ var moveTo = document.getElementById('sp' + player.location)
+ moveTo.style.backgroundImage = "url('" + player.tokensrc + "')";
+ moveTo.style.color = "transparent";
+ selectPlayerFunction(player.location, player, bank, dieRoll, players, chanceDeck);
 });
